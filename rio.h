6c0e0efd692b98feb006b1de98a630eed8630b35
Edward Ahn <edhyah@gmail.com>
2015-12-29 22:31:42 +0900

    implemented rio.h and rio.c

--- rio.h
+++ rio.h
@@ -1,3 +1,41 @@
 /*
- * rio.h - robust I/O wrappers
+ * rio.h - prototypes and definitions for robust I/O functions
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define RIO_BUFSIZE 8192
+typedef struct {
+    int rio_fd;                /* Descriptor for this internal buf */
+    int rio_cnt;               /* Unread bytes in internal buf */
+    char *rio_bufptr;          /* Next unread byte in internal buf */
+    char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
+} rio_t;
+
+extern int h_errno;    /* Defined by BIND for DNS errors */
+
+/* Error-handling function */
+void unix_error(char *msg);
+
+/* Unix I/O wrappers */
+int Open(const char *pathname, int flags, mode_t mode);
+void Close(int fd);
+
+/* Rio (Robust I/O) package */
+ssize_t rio_readn(int fd, void *usrbuf, size_t n);
+ssize_t rio_writen(int fd, void *usrbuf, size_t n);
+void rio_readinitb(rio_t *rp, int fd);
+ssize_t	rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+ssize_t	rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
+
+/* Wrappers for Rio package */
+ssize_t Rio_readn(int fd, void *usrbuf, size_t n);
+void Rio_writen(int fd, void *usrbuf, size_t n);
+void Rio_readinitb(rio_t *rp, int fd);
+ssize_t Rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
--- tracker.c
+++ tracker.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "rio.h"
 
 #define BUFLEN  20
 
